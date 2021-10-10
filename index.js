b589071c44a212bd2fcf4430488385cac597880a
Sophia Sales <sophiabrenda10@gmail.com>
2021-10-10 15:55:41 -0300

    POOJavascript = Metodos e variaveis privadas

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ class Cliente{ //classe igual a um molde
 }
 class ContaCorrente{
     agencia;
-    saldo;
+    _saldo = 0; //Saldo passa a ser um valor privado podendo ser acesado só dentro da classe  
 
 //metodo sacar
     sacar(valor){
-        if(this.saldo >= valor){//se o valor do saldo for maior e igual ao valor da conta = pode realizar o saque  
-            this.saldo -=valor;//se o valor for menor e igua a valor da conta = nao pode realizar o saque  
+        if(this._saldo >= valor){//se o valor do saldo for maior e igual ao valor da conta = pode realizar o saque  
+            this._saldo -=valor;//se o valor for menor e igua a valor da conta = nao pode realizar o saque  
+        }
+    }
+    depositar(valor){
+        if(valor >0){//se valor for maior que 0 = ele pode receber o deposito 
+            this._saldo += valor; //se o saldo for maior e igual ao valor ele pode receber o deposito, validaçao para nao receber valor negativo 
         }
     }
 }
@@ -25,14 +30,12 @@ cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
 
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.saldo = 100;
-console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.depositar(100);//usando o metodo depositar 
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
 contaCorrenteRicardo.sacar(50) //usando o metodo sacar em uma funçao e adicionando o valor em parentesses 
 
+console.log(contaCorrenteRicardo);
 
-console.log(cliente1);
-console.log(cliente2);
