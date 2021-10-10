52aa9ad1ea395a4aa1660e66da7226cb63ed86c7
Sophia Sales <sophiabrenda10@gmail.com>
2021-10-10 15:30:19 -0300

    POOJavascript = Metodos criando e usando

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 class Cliente{ //classe igual a um molde 
     nome;
     cpf;
+}
+class ContaCorrente{
     agencia;
     saldo;
-}
-//objeto usando os moldes da classe, new segnifica a criaçao de uma instancia da classe 
-const cliente1 = new Cliente() 
-const cliente2 = new Cliente()
 
+//metodo sacar
+    sacar(valor){
+        if(this.saldo >= valor){//se o valor do saldo for maior e igual ao valor da conta = pode realizar o saque  
+            this.saldo -=valor;//se o valor for menor e igua a valor da conta = nao pode realizar o saque  
+        }
+    }
+}
+
+//objeto usando os moldes da classe, new segnifica a criaçao de uma instancia da classe 
+const cliente1 = new Cliente();
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(50) //usando o metodo sacar em uma funçao e adicionando o valor em parentesses 
 
 
-const cliente3Nome = "Alice";
-const cliente3CPF = 88822233309;
-const cliente3Agencia = 1001;
-const cliente3ASaldo = 0;
-
-console.log(cliente1, cliente2);
+console.log(cliente1);
+console.log(cliente2);
